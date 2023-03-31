1cdc7dfded264eb5fc6796979925e97def9d3619
Pedro Gabriel Felix Da Silva <48746019871@info.local>
2023-03-31 15:00:20 -0300

    exercicio 6

---
+++ 1° Semestre/BackEnd/Desvios Condicionais - Exercícios 1 ao 6/Exercício 6/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine($"Informe sua média: ");
+float media = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe sua frequencia: ");
+float frequencia = float.Parse(Console.ReadLine());
+
+if ( ( frequencia >= 75 ) && ( media >= 3 ) && ( media <= 6 ) )
+{
+    Console.WriteLine($"O aluno está de recuperação.");
+    
+}
+else if ( ( frequencia >= 75 ) && ( media >= 7 ) )
+{
+    Console.WriteLine($"O aluno foi aprovado.");
+}
+else 
+{
+    Console.WriteLine($"O aluno foi reprovado.");
+}
