c3e2a94abea85600dfc9e75be723c0468f790c18
Pedro Felix <pedro.silva591@aluno.senai.br>
2023-08-22 17:18:40 -0300

    Att

--- 2º Semestre/2º API/Repositorio/webapi.filmes.tarde/Interfaces/IFilmeRepository.cs
+++ 2º Semestre/2º API/Repositorio/webapi.filmes.tarde/Interfaces/IFilmeRepository.cs
@@ -1,6 +1,49 @@
-﻿namespace webapi.filmes.tarde.Interfaces
+﻿using webapi.filmes.tarde.Domains;
+
+namespace webapi.filmes.tarde.Interfaces
 {
     public interface IFilmeRepository
     {
+        // CRUD
+
+        //TipoDeRetorno NomeMetodo(TipoParametro NomeParametro)
+
+        /// <summary>
+        /// Cadastrar um novo filme
+        /// </summary>
+        /// <param name="novofilme">Este parametro é o objeto que será cadastrado</param>
+        void Cadastrar(FilmeDomain novoFilme);
+
+        /// <summary>
+        /// Retornar todos os gêneros cadastrados
+        /// </summary>
+        /// <returns>Retorna uma lista de gêneros</returns>
+        List<FilmeDomain> ListarTodos();
+
+        /// <summary>
+        /// Buscar um obj atraves do seu id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        FilmeDomain BuscarPorId(int id);
+
+        /// <summary>
+        /// Atualizar um filme existente, passando o id pelo corpo da requisicao
+        /// </summary>
+        /// <param name="filme">Objeto filme com novas informacões</param>
+        void AtualizarIdCorpo(FilmeDomain filme);
+
+        /// <summary>
+        /// Atualizar um filme existente passando o id pela url da requisicao
+        /// </summary>
+        /// <param name="id">id do obj a ser att</param>
+        /// <param name="filme">obj com as novas infos</param>
+        void AtualizarIdUrl(int id, FilmeDomain filme);
+
+        /// <summary>
+        /// Deletar um filme existente
+        /// </summary>
+        /// <param name="id">id do obj a ser deletado</param>
+        void deletar(int id);
     }
 }
--- 2º Semestre/2º API/Repositorio/webapi.filmes.tarde/Repositories/GeneroRepository.cs
+++ 2º Semestre/2º API/Repositorio/webapi.filmes.tarde/Repositories/GeneroRepository.cs
@@ -1,10 +1,19 @@
-﻿using webapi.filmes.tarde.Domains;
+﻿using System.Data.SqlClient;
+using webapi.filmes.tarde.Domains;
 using webapi.filmes.tarde.Interfaces;
 
 namespace webapi.filmes.tarde.Repositories
 {
     public class GeneroRepository : IGeneroRepository
     {
+        /// <summary>
+        /// String de conexão com o banco de dados que recebe os seguintes parâmetros:
+        /// Data Source: Nome do servidor do banco
+        /// Initial Catalog: Nome do banco de dados
+        ///     - windows: Intrated Security = True
+        ///     - SqlServer: User id = sa; Pwd = Senai@134
+        /// </summary>
+        private string StringConexao = "Data Source = DESKTOP-2B634JF; Initial Catalog = FilmesTarde; User Id = sa; Pwd = Senai@134";
         public void AtualizarIdCorpo(GeneroDomain genero)
         {
             throw new NotImplementedException();
@@ -29,10 +38,55 @@ namespace webapi.filmes.tarde.Repositories
         {
             throw new NotImplementedException();
         }
-
+        
+        /// <summary>
+        /// Listar todos os objetos do tipo genero
+        /// </summary>
+        /// <returns>Lista de objetos do tipo genero</returns>
         public List<GeneroDomain> ListarTodos()
         {
-            throw new NotImplementedException();
+            //Cria uma lista de generos, onde será armazenados os generos
+            List<GeneroDomain> ListaGeneros= new List<GeneroDomain>();
+
+            //Declara a SqlConnection passando a string de conexão como parâmetro
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                // Declara a instrução a ser executada
+                string querySelectAll = "SELECT IdGenero, Nome FROM Genero";
+
+                //ABre a conexão com o banco de dados
+                con.Open();
+
+                //Declara o SqlDataReader para percorrer(ler) a tabela no banco de dados 
+                SqlDataReader rdr;
+
+                //Declara o sqlCommand passando a query que será executada e a conexão
+                using (SqlCommand cmd = new SqlCommand(querySelectAll, con))
+                {
+                    //Executa a query e armazena os dados no rdr
+                    rdr = cmd.ExecuteReader();
+
+                    //Enquanto houver resgistros à serem lidos no rdr o laço se repetirá
+                    while(rdr.Read()) 
+                    {
+                        //Instancia um objeto do tipo GeneroDOmain
+                        GeneroDomain genero = new GeneroDomain()
+                        {
+                            //Atribui a propriedade IdGenero à uma variavel, sendo ela o primeiro valor de uma tabela
+                            IdGenero = Convert.ToInt32(rdr[0]),
+
+                            //Atribui a propiedade nome o valor da coluna nome
+                            Nome = Convert.ToString(rdr["Nome"])
+                        };
+
+                        //Adiciona o objeto criado dentro da lista
+                        ListaGeneros.Add(genero);
+                    }
+                }
+                //Retorna a lista de generos 
+                return ListaGeneros;
+            }
         }
+        
     }
 }
